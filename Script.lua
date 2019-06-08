ac9e8545081242e88c588c267c1d2f2906236bbe
daviskyLV <51496244+daviskyLV@users.noreply.github.com>
2019-06-08 12:51:27 +0300

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,7 +1,7 @@
 a = script.Parent:WaitForChild("Sound")
 lastPlayed = 0 --time tick when it was last played
 script.Parent.Touched:Connect(function(hit)
-if tick() >= lastPlayed+60 and hit and hit.Parent and hit.Parent:FindFirstChildPfClass("Humanoid") then
+if tick() >= lastPlayed+60 and hit and hit.Parent and hit.Parent:FindFirstChildOfClass("Humanoid") then
 a:Play()
 lastPlayed = tick()
 end
